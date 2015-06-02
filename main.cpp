49dbb6978259e24c069a7ba528967d02e8ed1f3d
Joao Henrique <jonibeatles@gmail.com>
2015-06-02 17:15:57 -0300

    inicial

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<cpthread>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+//recebe por parametro o arquivo de processos
+int main(int argc, char** argv){
+	
+	
+	if(argc != 2){
+		return 1;
+	}
+
+	
+
+
+
+
+	return 0;
+}
